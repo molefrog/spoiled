54d5c15bb43e5bf394607ecdeb22653ecb3ac3cd
Alexey Taktarov <molefrog@gmail.com>
2024-02-08 12:22:01 +0200

    Move demo to its own directory.

---
+++ demo/demo.ts
@@ -0,0 +1,10 @@
+import "./demo.css";
+import { Spoiler } from "../src/index";
+
+for (const el of document.querySelectorAll("spoiler, .spoiler")) {
+  if (el instanceof HTMLElement) {
+    const accent = el.classList.contains("dark-theme") ? "rgb(200, 200, 200)" : undefined;
+
+    new Spoiler(el, { fps: 24 /*, accentColor: accent */ });
+  }
+}
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: "demo",
   css: {
     transformer: "lightningcss",
   },
