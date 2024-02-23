ed81fc4f9178565fa222a5cbdc7ac1c2bb9ebdab
Alexey Taktarov <molefrog@gmail.com>
2024-02-23 11:30:04 +0100

    Configure Vitest.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    root: "src",
+    environment: "happy-dom",
+  },
+
   plugins: [react()],
 
   root: "demo",
