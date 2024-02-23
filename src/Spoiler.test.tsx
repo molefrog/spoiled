978250597f2baa6c78eb292f4b42cb172274958e
Alexey Taktarov <molefrog@gmail.com>
2024-02-23 14:38:14 +0100

    Test content transitions.

--- src/Spoiler.test.tsx
+++ src/Spoiler.test.tsx
@@ -9,19 +9,23 @@ afterEach(cleanup);
 
 describe("wrapper element customization", () => {
   it("renders a span wrapper by default", () => {
-    const { getByText } = render(<Spoiler>Hello!</Spoiler>);
-    expect(getByText("Hello!")).toBeInTheDocument();
-    expect(getByText("Hello!").tagName).toBe("SPAN");
+    const { container } = render(<Spoiler>Hello!</Spoiler>);
+    const element = container.firstElementChild!;
+
+    expect(element).toBeInTheDocument();
+    expect(element.tagName).toBe("SPAN");
   });
 
   it("passes standard props to the element", () => {
-    const { getByText } = render(
+    const { container } = render(
       <Spoiler className="custom" dir="rtl">
         Hello!
       </Spoiler>
     );
-    expect(getByText("Hello!")).toHaveClass("custom");
-    expect(getByText("Hello!")).toHaveAttribute("dir", "rtl");
+    const element = container.firstElementChild!;
+
+    expect(element).toHaveClass("custom");
+    expect(element).toHaveAttribute("dir", "rtl");
   });
 
   it("can customize wrapper's tag via `tagName` prop", () => {
@@ -30,7 +34,9 @@ describe("wrapper element customization", () => {
         <img alt="test" />
       </Spoiler>
     );
-    const divElement = container.querySelector("div");
+
+    const divElement = container.firstElementChild!;
+
     expect(divElement).toBeInTheDocument();
     expect(divElement).toContainHTML('<img alt="test">');
   });
@@ -70,9 +76,13 @@ describe("controlled/uncontrolled Spoiler Component", () => {
     await waitFor(() => expect(changeHandler).toHaveBeenCalledWith(true));
   });
 
-  it("renders with aria-hidden=true when hidden is true", () => {
-    const { getByText } = render(<Spoiler hidden={true}>Always hidden</Spoiler>);
-    expect(getByText("Always hidden")).toHaveAttribute("aria-expanded", "false");
+  it("controls Spoiler's visibility via `hidden` prop", () => {
+    const { container, rerender } = render(<Spoiler hidden={true}>Always hidden</Spoiler>);
+
+    expect(container.firstElementChild!).toHaveAttribute("aria-expanded", "false");
+
+    rerender(<Spoiler hidden={false}>Always visible</Spoiler>);
+    expect(container.firstElementChild!).not.toHaveAttribute("aria-expanded", "false");
   });
 });
 
@@ -82,5 +92,32 @@ describe("Spoiler options", () => {
 
     // @ts-expect-error unknown option
     <Spoiler painterShouldExplode="1">Hey</Spoiler>;
+  });
+});
+
+describe("content transitions via `transition` keyword", () => {
+  it('renders one wrapper element when it is falsey or "none"', () => {
+    const { getByText } = render(
+      <Spoiler transition={false}>Spoiler Alert: Dumbledore joins Voldemort</Spoiler>
+    );
+
+    const element = getByText(/Voldemort/);
+
+    expect(element).toBeInTheDocument();
+    expect(element.tagName).toBe("SPAN");
+    expect(element.parentElement?.tagName).not.toBe("SPAN");
+  });
+
+  it("renders with specified transition", () => {
+    const { getByText } = render(
+      <Spoiler transition="iris">Spoiler Alert: Frodo keeps the Ring</Spoiler>
+    );
+
+    const element = getByText(/Frodo/);
+    expect(element).toBeInTheDocument();
+
+    // should be span within a span
+    expect(element.tagName).toBe("SPAN");
+    expect(element.parentElement?.tagName).toBe("SPAN");
   });
 });
